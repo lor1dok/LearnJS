86f91f80dabd00a2947b59b6ccc8fe2dee3dc8db
lor1dok <lord_binder@mail.ru>
2021-02-08 17:03:27 +0300

    Lesson_20-object-deconstruct

--- js/simple.js
+++ js/simple.js
@@ -1,25 +1,37 @@
 'use strict';
 
-function first() {
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border :'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+console.log(options.name);
+
+// delete options.name;
+
+console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    } 
 }
 
-function second() {
-    console.log(2);
-}
+console.log(Object.keys(options).length); //Выводим массив свойств первого уровня и считаем его длину
 
-first();
-second();
 
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done); //При передаче функции, она передается без ()
