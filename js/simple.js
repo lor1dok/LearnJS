004c3bc77bdd01b7805002eec5701749aea38dea
lor1dok <lord_binder@mail.ru>
2021-02-08 16:12:09 +0300

    Lesson_19-callback

--- js/simple.js
+++ js/simple.js
@@ -1,71 +1,25 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (numberOfFilms == '' || numberOfFilms == null ||
-        isNaN(numberOfFilms)) {
-            numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
+function first() {
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-// start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms () {
-    for (let i = 0; i < 2; i++) {
-        const lSeenFilm = prompt("Какой последний фильм вы посмотрели?", ''),
-              lSeenFilmRate = +prompt("Какую оценку вы ему бы поставили?", '');
-        if (lSeenFilm != null && lSeenFilmRate != null && 
-            lSeenFilm != '' && lSeenFilmRate != '' && 
-            lSeenFilm.length < 51) {
-            personalMovieDB.movies[lSeenFilm] = lSeenFilmRate;
-        } else {
-            console.log('error');
-            i--;
-        }    
-    }
+function second() {
+    console.log(2);
 }
 
-// rememberMyFilms();
+first();
+second();
 
-function detectPersonaLevel() {
-    if (personalMovieDB.count > 30) {
-        console.log("Да вы киноман");
-    } else if (personalMovieDB.count > 10) {
-        console.log("Вы любитель кино");
-    } else if (personalMovieDB.count > 0) {
-        console.log("Вы новичок");
-    } else {
-        console.log("Произошла какая-то ошибка!")
-    }
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
 
-// detectPersonaLevel();
-
-function writeYourGenres(objDB) {
-    let genreNumber = 0;
-    while (genreNumber < 2) {
-        genreNumber = objDB.genres.length;
-        objDB.genres[genreNumber] = prompt(`Ваш любымый жанр №${genreNumber + 1}?`, '')
-    }
+function done() {
+    console.log('Я прошел этот урок!');
 }
 
-writeYourGenres(personalMovieDB);
-
-function showMyDB(objHid) {
-    if (!objHid) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
+learnJS('JavaScript', done); //При передаче функции, она передается без ()
