315158ee398f9a42a6f9b93c7a34a712170f998f
lor1dok <lord_binder@mail.ru>
2021-02-12 17:57:54 +0300

    lesson_29-Elements-actions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,53 @@
 'use strict';
 
-const box = document.getElementById('box');
-console.log(box);
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = document.querySelector('.heart');
+      
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
+// box.style.backgroundColor = 'green';
+// box.style.width = '500px';
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+const num = 500;
+box.style.cssText = `background-color: green; width: ${num}px`;
 
-const hearts = document.querySelectorAll('.heart');
+btns[1].style.borderRadius = '20%';
+circles[2].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+document.querySelector('.wrapper').append(div);
+
+wrapper.append(div);
+// wrapper.prepend(div);
+// wrapper.appendChild(div); //Старый вариант, работает как и append
+
+// hearts[0].before(div);
+// hearts[1].after(div);
+wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]); //Старый вариант
+
+hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]); //Старый вариант
+
+div.innerHTML ="<h1>Hello World</h1>";
+
+// div.textContent = "<h1>Hello World</h1>"; //Теги уже пихать нельзя
+
+div.insertAdjacentHTML("afterend", `<h2>Hell' o</h2>`);
