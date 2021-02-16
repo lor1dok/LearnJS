e7b6570448e73d53e270d5532de5ad6410ffe304
lor1dok <lord_binder@mail.ru>
2021-02-16 11:15:33 +0300

    Lesson_32-DOM-nav

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-const btn = document.querySelector('button'),
-      btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
+"use strict";
 
-// btn.onclick = function() {
-//     alert('Click');
-// }; //Опять же почти не используется
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
 
-// btn.onclick = function() {
-//     alert('Second Click');
-// }; //Повторение заменяет предыдущее
-//     //Нельзя удалить событие после назначение
+// console.log(document.querySelector('#current').parentNode.parentNode);
 
-// btn.addEventListener('click', () => {
-//     alert('Click');
-// });
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-// btn.addEventListener('click', () => {
-//     alert('Second Click');
-// }); //Можно назначать несколько действий на одно событие
-//     //События в жабскрипт выполняются по очереди
+// console.log(document.querySelector('#current').parentElement);
 
-// btn.addEventListener('mouseenter', (e) => {
-//     console.log('Hover');
-//     console.log(e.target);
-//     e.target.remove();
-// });
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
 
-// let i = 0;
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // } //Можно заменить третьим аргументом опции, к примеру once, которое заменить функцию выше.
-};
-
-btn.addEventListener('click', deleteElement);
-overlay.addEventListener('click', deleteElement);
-//Всплытие по иерархии, сначала отработает внутренний, а затем внешний элемент
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-link.addEventListener('click', function(event) {
-    event.preventDefault(); //Отмена стандартного поведения браузера
-    
-    console.log(event.target);
-});
-
-
+    console.log(node);
+}
