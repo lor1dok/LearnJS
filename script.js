8376657cc4be9b788902bb2d520633231c149aff
lor1dok <lord_binder@mail.ru>
2021-02-19 20:18:20 +0300

    Lesson_40-dates

--- script.js
+++ script.js
@@ -1,47 +1,24 @@
 "use strict";
 
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+const now = new Date();
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+//SETTERS
+console.log(now.setHours(18));
+console.log(now);
 
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+//GETTERS
+// console.log(now.getMonth()); //Месяцы идут с нуля
+// console.log(now.getDay()); //День НЕДЕЛИ воскресенье - 0
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+const start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
 }
 
-btn.addEventListener('click', myAnimation);
-// btn.addEventListener('click', () => {
-//     // const timerId = setTimeout(logger, 2000);
-//     timerId = setInterval(logger, 500);
-// });
+let end = new Date();
 
-
-
-
-// clearInterval(timerId); //Отменили таймер
-
-// function logger () {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('text');
-//     i++;
-// }
-
-// let id = setTimeout(function log() {
-//     console.log('Hello');
-//     id = setTimeout(log, 500);
-// }, 500);
-// const timerId = setTimeout(logger, 2000); //Определяем Конкретный таймаут
-// setTimeout(logger, 2000); - тоже сработает.
+alert(`Цикол отработал за ${end - start} мс`);
