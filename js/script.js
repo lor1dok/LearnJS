e36e43d025e0ff9b2a8d650141b7f1705b9533aa
lor1dok <lord_binder@mail.ru>
2021-02-25 11:26:12 +0300

    Lesson48_Practice-init

--- js/script.js
+++ js/script.js
@@ -1,37 +1,143 @@
 "use strict";
 
-class Rectangle { //Классы пишутся с Большой_буквы
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+window.addEventListener('DOMContentLoaded', () => {
+
+    // TABS
+    const tabs = document.querySelectorAll(".tabheader__item"),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector(".tabheader__items");
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display ='none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
     }
 
-    calcArea() {
-        return this.height * this.width;
-    }
-} // Концепция или шаблон 
-
-class ColoredRectangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width); //Супер всегда должна идти первой строчкой
-        this.text = text;
-        this.bgColor = bgColor;
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
     }
 
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+               if (target == item) {
+                   hideTabContent();
+                   showTabContent(i);
+               } 
+            });
+        }
+    });
+
+    // Timer
+
+    const deadline = '2021-02-26';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(t / (1000 * 60 * 60) % 24),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        
+              return {
+                  'total': t,
+                  'days': days,
+                  'hours': hours,
+                  'minutes': minutes,
+                  'seconds': seconds
+              };
     }
-}
 
-const div = new ColoredRectangleWithText(16, 16, "Hell'o warld", 'black');
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        }   else {
+            return num;
+        }
+    }
 
-const squere = new Rectangle(32, 32); // Экземпляр
-const long = new Rectangle(20, 100); // Экземпляр
-// Абстракция, отделяем концепцию от экземпляров
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
 
+        updateClock(); // init, fixes on page refresh timer blink
 
-console.log(squere.calcArea());
-console.log(long.calcArea());
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
 
-div.showMyProps();
-console.log(div.calcArea());
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    // Modal
+
+    const modalBtn = document.querySelectorAll('[data-modal]');
+    const modalWindow = document.querySelector('.modal');
+
+    console.log(modalWindow);
+    console.log(modalBtn);
+
+    function openModal() {
+        document.body.style.overflow = 'hidden';
+        modalWindow.style.display = 'block';
+        clearInterval(modalTimerId);
+        window.removeEventListener('scroll', showModalByScroll);
+    }
+
+    function closeModal() {
+        document.body.style.overflow = '';
+        modalWindow.style.display = '';
+    }
+
+    modalBtn.forEach(element => {
+        console.log(modalWindow.style.display);
+        element.addEventListener ('click', () => {
+            if (modalWindow.style.display === '') {
+                openModal();
+            } else {
+                closeModal();
+            }
+        });
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modalWindow.style.display === 'block') {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= 
+            document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+});
