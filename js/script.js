ef6e428e3049b3d625c00afdde805324b6a03470
lor1dok <lord_binder@mail.ru>
2021-03-01 12:23:54 +0300

    Lesson054_Added-spinner

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2021-02-28';
+    const deadline = '2021-03-02';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -103,7 +103,7 @@ window.addEventListener('DOMContentLoaded', () => {
     function openModal() {
         document.body.style.overflow = 'hidden';
         modalWindow.style.display = 'block';
-        // clearInterval(modalTimerId);
+        clearInterval(modalTimerId);
         window.removeEventListener('scroll', showModalByScroll);
     }
 
@@ -111,6 +111,13 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
         modalWindow.style.display = '';
     }
+
+    modalWindow.addEventListener('click', (e) => {
+        if(e.target === modalWindow || 
+           e.target.getAttribute('data-close') == '') {
+            closeModal();
+        }
+    });
 
     modalBtn.forEach(element => {
         console.log(modalWindow.style.display);
@@ -129,7 +136,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // const modalTimerId = setTimeout(openModal, 5000);
+    const modalTimerId = setTimeout(openModal, 50000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= 
@@ -230,6 +237,8 @@ window.addEventListener('DOMContentLoaded', () => {
         postData(item);
     });
 
+    // POST Form
+    
     function postData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -257,18 +266,40 @@ window.addEventListener('DOMContentLoaded', () => {
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success);
                     form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
                 } else {
                     statusMessage.textContent = message.failure;
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
+                    showThanksModal(message.failure);
                 }
             });
         });
+    }
+
+    // POST Form END
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.style.display = 'none';
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>&times;</div>
+                <div class ="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.style.display = '';
+            closeModal();
+        }, 4000);
     }
 });
